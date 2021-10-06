30d0ee525e2444697b358d313287a6e78fba7370
Matt Brace <ismarc31@gmail.com>
2021-10-05 21:35:35 -0500

    Add rudimentary config loading

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,12 +1,51 @@
 package app
 
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 type Config struct {
-	configPath string
+	DataDirectory string `yaml:"data-directory"`
 }
 
 func LoadConfig(location string) (config *Config, err error) {
-	config = &Config{
-		configPath: location,
+	log.Printf("Loading config from %s\n", location)
+	config = &Config{}
+	if _, err := os.Stat(location); os.IsNotExist(err) {
+		config = loadDefaults(config)
+	} else {
+		config, err = loadConfig(config, location)
 	}
+	return
+}
+
+func loadConfig(c *Config, location string) (config *Config, err error) {
+	file, err := os.Open(location)
+	if err != nil {
+		log.Printf("Error loading config file '%s'\n", location)
+		return
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(c)
+	if err != nil {
+		log.Print("Error parsing config file\n")
+	}
+
+	config = loadDefaults(c)
+	return
+}
+
+func loadDefaults(c *Config) (config *Config) {
+	if c.DataDirectory == "" {
+		c.DataDirectory = "~/.defnodo"
+	}
+
+	config = c
+
 	return
 }
