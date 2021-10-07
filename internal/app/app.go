560fe39fe77ea6f03b285f0a328a0fc8d5083059
Matt Brace <ismarc31@gmail.com>
2021-10-07 18:23:09 -0500

    Add interactive/foreground start

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,11 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
 
 type DefNoDo struct {
 	config *Config
@@ -17,6 +22,37 @@ func NewDefNoDoService(config *Config) (defnodo *DefNoDo) {
 // on the supplied runtime config.
 func (defnodo *DefNoDo) Start() (err error) {
 	fmt.Printf("Starting service with config: %+v\n", defnodo.config)
+	linuxkitPath, err := exec.LookPath("linuxkit")
+	// vpnkitPath, err = exec.LookPath("vpnkit")
+	if err != nil {
+		log.Print("Unable to find linuxkit binary")
+		return
+	}
+	// ./linuxkit run hyperkit -fw bunk_uefi.fd  -disk size=4G -networking=vpnkit -vsock-ports 2376 -kernel  -data-file metadata.json -mem 4096 docker-for-mac
+	err = os.Chdir(defnodo.config.DataDirectory)
+	if err != nil {
+		os.MkdirAll(defnodo.config.DataDirectory, 666)
+	}
+	cmd := exec.Command(linuxkitPath,
+		"run", "hyperkit",
+		"-fw", "bunk_uefi.fd",
+		"-disk", "size=4G",
+		"-networking", "vpnkit",
+		"-vsock-ports", "2376",
+		"-kernel",
+		"-data-file", "metadata.json",
+		"-mem", "4096",
+		"-state", "defnodo-state",
+		"defnodo")
+	cmd.Env = os.Environ()
+
+	log.Printf("Starting linuxkit: %v", cmd.Args)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
 	return
 }
 
