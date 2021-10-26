3a5c8dd98c12aa7c100e2e459ac8e289725fba90
Matt Brace <ismarc31@gmail.com>
2021-10-26 18:47:51 -0500

    Duct tape in simple directory mounting and running

--- go.mod
+++ go.mod
@@ -8,9 +8,14 @@ require (
 )
 
 require (
+	9fans.net/go v0.0.2 // indirect
+	github.com/Plan9-Archive/libauth v0.0.0-20180917063427-d1ca9e94969d // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	github.com/fhs/mux9p v0.3.1 // indirect
 	github.com/kardianos/service v1.2.0 // indirect
+	github.com/knusbaum/go9p v1.18.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+	golang.org/x/sys v0.0.0-20201020230747-6e5568b54d1a // indirect
 )
--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/ismarc/defnodo/internal/serve9p"
 	"github.com/kardianos/service"
 )
 
@@ -35,28 +36,41 @@ func (defnodo *DefNoDo) Stop(s service.Service) error {
 // Run the defnodo service
 func (defnodo *DefNoDo) Run() (err error) {
 	fmt.Printf("Starting service with config: %+v\n", defnodo.config)
-	linuxkitPath, err := exec.LookPath("linuxkit")
+
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileShare, err := serve9p.NewServe9P("tcp://127.0.0.1:7777", []string{homedir}, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go fileShare.Run()
+
+	// linuxkitPath, err := exec.LookPath("linuxkit")
 	// vpnkitPath, err = exec.LookPath("vpnkit")
-	if err != nil {
-		log.Print("Unable to find linuxkit binary")
-		return
-	}
+	// if err != nil {
+	// 	log.Print("Unable to find linuxkit binary")
+	// 	return
+	// }
+
 	// ./linuxkit run hyperkit -fw bunk_uefi.fd  -disk size=4G -networking=vpnkit -vsock-ports 2376 -kernel  -data-file metadata.json -mem 4096 docker-for-mac
-	err = os.Chdir(defnodo.config.DataDirectory)
-	if err != nil {
-		os.MkdirAll(defnodo.config.DataDirectory, 666)
-	}
-	cmd := exec.Command(linuxkitPath,
-		"run", "hyperkit",
-		"-fw", "bunk_uefi.fd",
-		"-disk", "size=4G",
-		"-networking", "vpnkit",
-		"-vsock-ports", "2376",
-		"-kernel",
-		"-data-file", "metadata.json",
-		"-mem", "4096",
-		"-state", "defnodo-state",
-		"defnodo")
+	// err = os.Chdir(defnodo.config.DataDirectory)
+	// if err != nil {
+	// 	os.MkdirAll(defnodo.config.DataDirectory, 666)
+	// }
+	cmd := exec.Command("./scripts/run_vm.sh")
+	// cmd := exec.Command(linuxkitPath,
+	// 	"run", "hyperkit",
+	// 	"-fw", "bunk_uefi.fd",
+	// 	"-disk", "size=4G",
+	// 	"-networking", "vpnkit",
+	// 	"-vsock-ports", "2376",
+	// 	"-kernel",
+	// 	"-data-file", "metadata.json",
+	// 	"-mem", "4096",
+	// 	"-state", "defnodo-state",
+	// 	"defnodo")
 	cmd.Env = os.Environ()
 
 	log.Printf("Starting linuxkit: %v", cmd.Args)
